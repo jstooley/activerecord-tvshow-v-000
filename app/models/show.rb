c127fbf00708f7475dfc15f2ddecfc3d7a20fc9e
Justin Tooley <jstooley823@gmail.com>
2017-11-05 16:33:18 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,9 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
   def self.most_popular_show
-    Show.where("rating = ?", Show.maximum(:rating))
+    self.where("rating = ?", Show.maximum(:rating))
   end
   def self.lowest_rating
   end
