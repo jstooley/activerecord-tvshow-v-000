efe0aa925e2d13bd6a28fd4634deccf4f3390800
Justin Tooley <jstooley823@gmail.com>
2017-11-05 16:35:39 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,8 @@ class Show < ActiveRecord::Base
   end
   def self.most_popular_show
     show = self.where("rating = ?", Show.maximum(:rating))
-    show[0]
+    
+    show[0] # becasue where returns result as array 
   end
   def self.lowest_rating
   end
