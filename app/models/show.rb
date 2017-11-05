1b1d24367ae74eaab7d7abedd0327d30f226ff1e
Justin Tooley <jstooley823@gmail.com>
2017-11-05 16:29:35 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
   def self.most_popular_show
-    Show.highest_rating
+    Show.highest_rating.name
   end
   def self.lowest_rating
   end
