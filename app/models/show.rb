fc50fe8eba6ac704026261d28b728aae943e8950
Justin Tooley <jstooley823@gmail.com>
2017-11-05 16:27:54 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,18 +1,18 @@
 class Show < ActiveRecord::Base
-  def highest_rating
+  def self.highest_rating
     Shows.maximum(:rating)
   end
-  def most_popular_show
+  def self.most_popular_show
     Show.maximum(:rating).title
   end
-  def lowest_rating
+  def self.lowest_rating
   end
-  def least_popular_show
+  def self.least_popular_show
   end
-  def ratings_sum
+  def self.ratings_sum
   end
-  def popular_shows
+  def self.popular_shows
   end
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
   end
 end
